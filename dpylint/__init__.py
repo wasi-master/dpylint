0082174fa6b47ad7e8fbf4b81eaa62b9576972c6
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2021-08-04 22:14:36 +0600

    make the main module

---
+++ dpylint/__init__.py
@@ -0,0 +1,12 @@
+"""Discord.py Linter"""
+__all__ = ("register",)
+__title__ = "dpylint"
+__version__ = "0.0.1"
+
+from .checkers import HasPermissionsChecker, EventsChecker
+
+
+def register(linter):
+    """Registers the linter to pylint"""
+    linter.register_checker(HasPermissionsChecker(linter))
+    linter.register_checker(EventsChecker(linter))
