463a564433dfccc237b426a4e9fa5ab32c7e0792
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2021-08-05 12:15:02 +0600

    Create shorthands.py

---
+++ dpylint/assets/shorthands.py
@@ -0,0 +1,11 @@
+# now I know this is probably not the right way but I don't know any other way to do this
+SHORTHANDS = {
+    "ctx.channel.send": "ctx.send",
+    "ctx.author.guild": "ctx.guild",
+    "ctx.message.author": "ctx.author",
+    "ctx.message.author.guild": "ctx.author",
+    "ctx.message.guild": "ctx.guild",
+    "ctx.message.channel":"ctx.channel",
+    "ctx.message.channel.guild":"ctx.guild",
+    "ctx.channel.pins":"ctx.pins"
+}
